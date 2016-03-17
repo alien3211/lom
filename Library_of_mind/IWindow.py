eb6b29fd3b88a80c6b89045a8d5626d6943f193d
Alan Tetich <alan.tetich@ericsson.com>
2016-03-17 13:02:43 +0100

    update windowsGTK

--- Library_of_mind/IWindow.py
+++ Library_of_mind/IWindow.py
@@ -4,9 +4,11 @@ import pkgutil
 def getWindow(a):
     log.LOG("IN GETWINDOW")
     if pkgutil.find_loader('gtk') is not None: 
+        log.LOG("import GTK")
         import WindowGTK
         return WindowGTK.Window()
     elif pkgutil.find_loader('PyQt') is not None: 
+        log.LOG("import QT")
         import WindowQT
         return WindowQT.Window()
 
--- Library_of_mind/WindowGTK.py
+++ Library_of_mind/WindowGTK.py
@@ -3,4 +3,74 @@
 
 import log
 
+import pygtk
+pygtk.require('2.0')
+import gtk
+import pango
 
+class Window(gtk.Window):
+
+    def __init__(self):
+        pass
+
+    def main_loop(self):
+        "Run main loop"
+        gtk.main()
+
+    def gtk_quit(self):
+        "Terminate Window"
+        gtk.main_quit()
+
+    def parserArgs(args):
+
+        rest = arg.split()
+        command = rest.pop(-1)
+
+        if command in ['help', 'h']:
+            return getHelp(rest)
+        elif command in ['set']:
+            return setOption(rest)
+        elif command in ['search', 's']:
+            return search(rest)
+        elif command in ['add','a']:
+            return addRecord()
+        elif command in ['update', 'u']:
+            return updateRecord(rest)
+        elif command in ['type', 't']:
+            return getType(rest)
+        elif command in ['key', 'k']:
+            return getKey(rest)
+        elif command in ['news', 'n']:
+            return getNews(rest)
+        elif command.isdigit():
+            return getDigit(int(command))
+
+    def getHelp(com):
+
+        return ConMySQL.getHelp(com)
+
+    def search(com):
+        pass
+
+    def addRecord(com):
+        pass
+
+    def getType(com):
+
+        if com:
+            return ConMySQL.getType(com)
+        else:
+            return ConMySQL,getType()
+
+    def getKey(com):
+
+        if com:
+            return ConMySQL.getKey(com)
+        else:
+            return ConMySQL,getKey()
+
+    def getNews(com):
+        pass
+
+    def getDigit(com):
+        pass
